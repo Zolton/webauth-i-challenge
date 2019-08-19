dead270b491e0ef39591d3e5e97e5a1e340ca064
Zolton <gmzesq@gmail.com>
2019-08-19 15:04:40 -0400

    tables set up, seeds run

--- knexfile.js
+++ knexfile.js
@@ -6,6 +6,11 @@ module.exports = {
     connection: {
       filename: "./data/users.db3"
     },
-    useNullAsDefault: true
+    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
+    }
   }
 };
--- migrations/20190819145948_users.js
+++ migrations/20190819145948_users.js
@@ -1,13 +1,17 @@
-
 exports.up = function(knex) {
-    return knex.schema.createTable("users", tbl=>{
-        tbl.increments()
-        tbl.string("userName", 128).unique().notNullable()
-        tbl.string("password", 128).unique().notNullable()
-    })
-  };
-  
-  exports.down = function(knex) {
-      return knex.schema.dropTableIfExists("users")
-  };
-  
+  return knex.schema.createTable("users", tbl => {
+    tbl.increments();
+    tbl
+      .string("userName", 128)
+      .unique()
+      .notNullable();
+    tbl
+      .string("password", 128)
+      .unique()
+      .notNullable();
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("users");
+};
