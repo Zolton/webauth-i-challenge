fca67a6c0a5a01a3ab42cc86d9a4d1f71e923e78
Zolton <gmzesq@gmail.com>
2019-08-19 16:32:35 -0400

    Password hashing works, GET users for only verified users works too

--- helperFunctions/db-helperFunctions.js
+++ helperFunctions/db-helperFunctions.js
@@ -1,7 +1,7 @@
 const db = require("../db-Config");
 const bcrypt = require("bcryptjs");
 
-module.exports = { getAllUsers, addNewUser, addHash };
+module.exports = { getAllUsers, addNewUser, addHash, validator };
 
 function getAllUsers() {
   return db("users");
@@ -15,5 +15,29 @@ function addHash(req, res, next) {
   const user = req.body;
   const hash = bcrypt.hashSync(user.password);
   user.password = hash;
-  next()
+  next();
+}
+
+function validator(req, res, next) {
+  const { username, password } = req.headers;
+  if (username && password) {
+    getUser({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compare(password, user.password)) {
+          next();
+        } else {
+          res.status(401).json({ Error: "invalid credentials" });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      })
+  } else {
+    res.status(400).json({ Error: "Please enter a username and password" });
+  }
+}
+
+function getUser(user) {
+  return db("users").where(user);
 }
--- userRouter/userRouter.js
+++ userRouter/userRouter.js
@@ -6,7 +6,7 @@ router.get("/", (req, res) => {
   res.status(200).json({ Hello: "From userRouter" });
 });
 
-router.get("/users", (req, res) => {
+router.post("/users", User.validator, (req, res) => {
   User.getAllUsers()
     .then(users => {
       res.status(200).json(users);
@@ -26,5 +26,15 @@ router.post("/register", User.addHash, (req, res) => {
       res.status(500).json({ Error: "Server status: 500" });
     });
 });
+
+// router.post("/login", (req, res) => {
+//   User.addNewUser
+//     .then(logged => {
+//       res.status(200).json({ Status: "Logged in" });
+//     })
+//     .catch(error => {
+//       res.status(500).json({ Error: "Server status: 500" });
+//     });
+// });
 
 module.exports = router;
