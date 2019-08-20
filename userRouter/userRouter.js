c7b2246e793786bfa17c4878c12f3d97027e8f31
Zolton <gmzesq@gmail.com>
2019-08-20 16:09:13 -0400

    Finished MVP

--- userRouter/userRouter.js
+++ userRouter/userRouter.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 const User = require("../helperFunctions/db-helperFunctions");
 
+// Route: 8000/api/---
+
 router.get("/", (req, res) => {
   res.status(200).json({ Hello: "From userRouter" });
 });
 
-router.post("/users", User.validator, (req, res) => {
+router.get("/users", User.restricted, (req, res) => {
   User.getAllUsers()
     .then(users => {
       res.status(200).json(users);
@@ -16,25 +18,5 @@ router.post("/users", User.validator, (req, res) => {
     });
 });
 
-router.post("/register", User.addHash, (req, res) => {
-  const newUser = req.body;
-  User.addNewUser(newUser)
-    .then(newU => {
-      res.status(200).json(newU);
-    })
-    .catch(error => {
-      res.status(500).json({ Error: "Server status: 500" });
-    });
-});
-
-// router.post("/login", (req, res) => {
-//   User.addNewUser
-//     .then(logged => {
-//       res.status(200).json({ Status: "Logged in" });
-//     })
-//     .catch(error => {
-//       res.status(500).json({ Error: "Server status: 500" });
-//     });
-// });
 
 module.exports = router;
